0f78519ef66fac833c2baa232a278dc36a2e6822
J0t4s <joaulo223@gmail.com>
2022-09-16 17:28:53 -0300

    fixed the css

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const btnSearch = document.getElementById('btn-search');
 const movieSpot = document.getElementById('movie-spot');
 const addBtn = document.getElementById('add/remove-watchlist-id');
 const startCont = document.getElementById('start-container');
-
 let moviesApiData = []
 let moviesAdded = []
 let movieFromStorage = JSON.parse(localStorage.getItem('watchlist'))
+
 if(movieFromStorage) {
     moviesAdded = [...movieFromStorage]
-    console.log(movieFromStorage)
 }
-
 
 async function getMoviesDataBase() {
     const res = await fetch(`https://www.omdbapi.com/?s=${inputSearch.value}&page=1&apikey=e2fe7aa4`)
     const data = await res.json()
-    console.log(data)
     if(data.response === false) {
         movieSpot.innerHTML = `
             <div class="error-container">
@@ -41,7 +38,7 @@ async function getMovies(moviesId) {
             console.log(err)
                 movieSpot.innerHTML = `
                     <div class="error-container">
-                    <h2>Unable to find what you’re looking for. Please try another search</h2>
+                        <h2>Unable to find what you’re looking for. Please try another search</h2>
                     </div>
                 `
         }
@@ -55,17 +52,24 @@ function renderMovies() {
             movieSpot.innerHTML +=
             `
                 <div  class="movie-container"> 
-                
-                <img src="${movie.Poster}" alt="poster of the movie ${movie.Title}" class="img-poster" id="poster">
-                <div class="movie-data">
-                    <h3 class="movie-title" id="movie-title">${movie.Title}</h3>
-                    <p class="movie-rating" id="rating">${movie.Ratings[0].Value}</p>
-                    <div class="movie-genre-container" >
-                        <p class="movie-run-time" id="run-time">${movie.Runtime}</p>
-                        <p class="movie-genre" id="genre">${movie.Genre}</p>
-                        <button class="watchlist plus-icon" id="${movie.imdbID}">Wachtlist</button>
-                    </div> 
-                    <p class="movie-plot" id="plot">${movie.Plot}</p>
+                    <img src="${movie.Poster}" alt="poster of the movie ${movie.Title}" class="img-poster" id="poster">
+                    <div class="movie-data">
+                        <h3 class="movie-title" id="movie-title">${movie.Title}</h3>
+                        <p class="movie-rating" id="rating">${movie.Ratings[0].Value}</p>
+                        <div class="movie-genre-container" >
+                            <p class="movie-run-time" id="run-time">${movie.Runtime}</p>
+                            <p class="movie-genre" id="genre">${movie.Genre}</p>
+                            <button class="watchlist" id="${movie.imdbID}">
+                                <i class="plus-icon">
+                                    <svg width="10" height="10" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" fill="white"/>
+                                    </svg>
+                                </i>
+                                Add to Wachtlist
+                            </button>
+                        </div> 
+                        <p class="movie-plot" id="plot">${movie.Plot}</p>
+                    </div>
                 </div>
             `
         )
@@ -88,10 +92,7 @@ function addToLocalStorage(id) {
     const addMovie = moviesApiData.find(el => el.imdbID === id)
     moviesAdded.push(addMovie)
     localStorage.setItem('watchlist', JSON.stringify(moviesAdded));
-    console.log(movieFromStorage)
 }
-
-
 
 function isAdded(id) {
     if (localStorage.getItem(id) === null) {
@@ -99,5 +100,4 @@ function isAdded(id) {
     } else {
       return false
     }
-  }
-
+}
--- watchlist.js
+++ watchlist.js
@@ -1,44 +1,50 @@
 
 const movieSpot = document.getElementById('movie-spot');
 const errorCont = document.getElementById('error-cont')
-
-
 let myWatchlistStorage = JSON.parse(localStorage.getItem('watchlist'))
-console.log(myWatchlistStorage)
 let movies = myWatchlistStorage;
 
-console.log(movies)
 renderMovies(movies);
 function renderMovies (watchlistMovie) {
     if(myWatchlistStorage !== null){
-        let moviesEl = movies.map(movie => {    
+        let moviesEl = movies.map(movie => { 
+
             return( `
-                <div  class="movie-container"> 
-                
-                <img src="${movie.Poster}" alt="poster of the movie ${movie.Title}" class="img-poster" id="poster">
-                <div class="movie-data">
-                    <h3 class="movie-title" id="movie-title">${movie.Title}</h3>
-                    <p class="movie-rating" id="rating">${movie.Ratings[0].Value}</p>
-                    <div class="movie-genre-container" >
-                        <p class="movie-run-time" id="run-time">${movie.Runtime}</p>
-                        <p class="movie-genre" id="genre">${movie.Genre}</p>
-                        <button class="watchlist remove-icon" id="${movie.imdbID}">Wachtlist</button>
-                    </div> 
-                    <p class="movie-plot" id="plot">${movie.Plot}</p>
+                <div clas="movie-storage">
+                    <div  class="movie-container"> 
+                    <img src="${movie.Poster}" alt="poster of the movie ${movie.Title}" class="img-poster" id="poster">
+                    <div class="movie-data">
+                        <h3 class="movie-title" id="movie-title">${movie.Title}</h3>
+                        <p class="movie-rating" id="rating">${movie.Ratings[0].Value}</p>
+                        <div class="movie-genre-container" >
+                            <p class="movie-run-time" id="run-time">${movie.Runtime}</p>
+                            <p class="movie-genre" id="genre">${movie.Genre}</p>
+                            <button class="watchlist" id="${movie.imdbID}">
+                                <i class="remove-icon">
+                                    <svg width="10" height="10" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z" fill="white"/>
+                                    </svg>
+                                </i>
+                                Remove from Wachtlist
+                            </button>
+                        </div> 
+                        <p class="movie-plot" id="plot">${movie.Plot}</p>
+                    </div>
                 </div>
             `
             )
-    })
+        })
         movieSpot.innerHTML = moviesEl
         movieSpot.classList.remove('invisible');        
+    }else{
         errorCont.classList.add('invisible');
     }
-
 }
 
 document.addEventListener('click', e => {
     if(e.target.matches('.watchlist')) {
             addEventListener("click", removeFromLocalStorage(e.target.id))
+            renderMovies(movies)
     }
 })
 
@@ -46,7 +52,5 @@ function removeFromLocalStorage(id) {
     const removeMovie = movies.find(el => el.imdbID === id)
     const indexMovie = movies.findIndex(movie => movie.imdbID === removeMovie.imdbID)
     movies.splice(indexMovie, 1)
-    console.log(movies)
     localStorage.setItem('watchlist', JSON.stringify(movies));
-    // console.log(movieFromStorage)
 }
